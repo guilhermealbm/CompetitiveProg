be9a0dd5b72760750b659206591bed5eaca21a6c
guilhermealbm <gui.miranda.13@gmail.com>
2022-05-09 19:54:45 -0300

    add 1001 and update gitignore

---
+++ 1001/1001.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     
+     int num1;cin>>num1;
+     int num2;cin>>num2;
+     
+     cout << "X = " << num1 + num2 << endl;
+ 
+    return 0;
+}
