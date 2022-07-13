51ee66bc016c910d900e1390bd444725bd64b350
guilhermealbm <gui.miranda.13@gmail.com>
2022-07-13 20:46:41 -0300

    add 1464

---
+++ List4/1464/1464.cpp
@@ -0,0 +1,144 @@
+//#include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fLL
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+struct pt { // ponto
+	int x, y;
+	pt(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
+	bool operator < (const pt p) const {
+		if (x != p.x) return x < p.x;
+		return y < p.y;
+	}
+	bool operator == (const pt p) const {
+		return x == p.x and y == p.y;
+	}
+	pt operator + (const pt p) const { return pt(x+p.x, y+p.y); }
+	pt operator - (const pt p) const { return pt(x-p.x, y-p.y); }
+	pt operator * (const int c) const { return pt(x*c, y*c); }
+	ll operator * (const pt p) const { return x*(ll)p.x + y*(ll)p.y; }
+	ll operator ^ (const pt p) const { return x*(ll)p.y - y*(ll)p.x; }
+	friend istream& operator >> (istream& in, pt& p) {
+		return in >> p.x >> p.y;
+	}
+};
+
+ll sarea2(pt p, pt q, pt r) { // 2 * area com sinal
+	return (q-p)^(r-q);
+}
+
+bool ccw(pt p, pt q, pt r) { // se p, q, r sao ccw
+	return sarea2(p, q, r) >= 0;
+}
+
+vector<pt> convex_hull(vector<pt> v) { // convex hull - O(n log(n))
+	if (v.size() <= 1) return v;
+	vector<pt> l, u;
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		while (l.size() > 1 and !ccw(l[l.size()-2], l.back(), v[i]))
+			l.pop_back();
+		l.push_back(v[i]);
+	}
+	for (int i = v.size() - 1; i >= 0; i--) {
+		while (u.size() > 1 and !ccw(u[u.size()-2], u.back(), v[i]))
+			u.pop_back();
+		u.push_back(v[i]);
+	}
+	l.pop_back(); u.pop_back();
+	for (pt i : u) l.push_back(i);
+	return l;
+}
+ 
+int main() { _
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    int n;
+
+    while (cin >> n) {
+        if (n == 0) return 0;
+
+        vector<pt> points;
+
+        for (int i = 0; i < n; i++) {
+            int x, y; cin >> x >> y;
+            points.push_back(pt(x, y));
+        }
+
+        int sum = 0;
+        while (points.size() != 0) {
+            vector<pt> hull = convex_hull(points);
+            if (hull.size() < 3) {
+                sum = 1;
+                break;
+            }
+
+            vector<pt> difference;
+            sort(points.begin(), points.end());
+            sort(hull.begin(), hull.end());
+            set_difference(
+                points.begin(), points.end(),
+                hull.begin(), hull.end(),
+                std::back_inserter( difference )
+            );
+
+            // cout << points.size() << "  " << hull.size() << "   " << difference.size() << endl; 
+
+            points = difference;
+            sum++;
+            
+        }
+
+        if (sum % 2) {
+            cout << "Take this onion to the lab!" << endl;
+        } else {
+            cout << "Do not take this onion to the lab!" << endl;
+        }
+        
+
+    }
+ 
+    return 0;
+}
